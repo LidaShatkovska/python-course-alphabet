8e92159230b32c45068f533bd780bc60f9a8017e
LidaShatkovska <lida.mudruk@gmail.com>
2019-05-06 00:51:10 +0300

    HW4_add_compare_cesars_and_other_fix

--- objects_and_classes/homework/homework.py
+++ objects_and_classes/homework/homework.py
@@ -53,35 +53,29 @@ class Car:
 
 
     def __str__(self):
-        return f"Car produser: {self.producer}, type: {self.type}, number: {self.number}, price: {self.price}, spend mileage: {self.mileage}"
+        return f"\nCar produser: {self.producer}, type: {self.type}, " \
+               f"number: {self.number}, price: {self.price}, spend mileage: {self.mileage}"
+
 
     def __repr__(self):
         return self.__str__()
 
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-
-        if self.current < len(self.cars):
-            res = self.cars[self.current]
-            self.current += 1
-            return res
-        else:
-            self.current = 0
-            raise StopIteration
 
     def __le__(self, other):
         return self.price <= other.price
 
+
     def __lt__(self, other):
         return self.price < other.price
+
 
     def __eq__(self, other):
         return self.price == other.price
 
+
     def __ne__(self, other):
         return self.price != other.price
+
 
     def change_UUID(self):
         self.number = uuid.uuid4()
@@ -98,25 +92,15 @@ class Garage:
         self.owner = owner
         self.current = 0
 
+
     def __str__(self):
         all_cars = "".join(str(item) for item in self.cars)
-        return f"Town: {self.town}, Places: {self.places}, car: {self.cars} ,owner: {self.owner}"
+        return f"\nTown: {self.town}, Places: {self.places}, owner: {self.owner}, car: {self.cars}"
+
 
     def __repr__(self):
         return self.__str__()
 
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-
-        if self.current < len(self.cars):
-            res = self.cars[self.current]
-            self.current += 1
-            return res
-        else:
-            self.current = 0
-            raise StopIteration
 
     def add(self,new_car):
         if len(self.cars) < self.places:
@@ -129,8 +113,9 @@ class Garage:
     def remove(self,my_car):
         if my_car in self.cars:
             self.cars.remove(my_car)
-            print('THE CAR {} WAS DELETED'.format(my_car))
+            print('THE CAR WAS DELETED!')
         return self
+
 
     def hit_hat(self):
         return sum(car.price for car in self.cars)
@@ -145,16 +130,27 @@ class Cesar:
         self.cesar_garages = cesar_garages if cesar_garages is not None else []
         self.register_id = uuid.uuid4()
 
+
     def __str__(self):
-        all_garages = "".join(str(item) for item in self.cesar_garages)
-        return f"Name: {self.name}, Garage: {self.cesar_garages}, Register_id: {self.register_id}"
+        return f"CESAR NAME: {self.name}, " \
+               f"\nREGISTER_ID: {self.register_id}, " \
+               f"\nCOUNT OF GARAGE: {self.garages_count()}, " \
+               f"\nCOUNT CAR IN ALL GARAGES:  {self.сars_count()}, " \
+               f"\nSUM PRICE ALL CAR:  {self.hit_hat()}" \
+               f"\nGARAGE: {self.cesar_garages}, "
+
+
+    def compare_cesar(cesar_list):
+        return max((cesar for cesar in cesar_list), key=lambda x: x.hit_hat() )
 
 
     def hit_hat(self):
         return sum(item.hit_hat() for item in self.cesar_garages)
 
+
     def garages_count(self):
         return len(self.cesar_garages)
+
 
     def сars_count(self):
         return sum(len(garage.cars) for garage in self.cesar_garages)
@@ -172,107 +168,107 @@ class Cesar:
 if __name__ == "__main__":
 
     cars = []
-    for _ in range(random.randint(10,100)):
-        car = Car(price=random.uniform(100.5,999.5),
-                   type=random.choice(CARS_TYPES),
-                   producer=random.choice(CARS_PRODUCER),
-                   mileage=random.uniform(0, 1000000),
-                   )
+    for car_counter in range(random.randint(10, 100)):
+        car = Car(price=random.uniform(100.5, 999.5),
+                  type=random.choice(CARS_TYPES),
+                  producer=random.choice(CARS_PRODUCER),
+                  mileage=random.uniform(0, 1000000),
+                  )
         cars.append(car)
 
-    car1 = Car(price=random.uniform(100.5,999.5),
-           type=random.choice(CARS_TYPES),
-            producer=random.choice(CARS_PRODUCER),
-            mileage=random.uniform(0, 1000000),
-            )
-    car2 = Car(price=random.uniform(100.5,999.5),
-           type=random.choice(CARS_TYPES),
-            producer=random.choice(CARS_PRODUCER),
-            mileage=random.uniform(0, 1000000),
-            )
+    car1 = Car(price=random.uniform(100.5, 999.5),
+               type=random.choice(CARS_TYPES),
+               producer=random.choice(CARS_PRODUCER),
+               mileage=random.uniform(0, 1000000),
+               )
+    car2 = Car(price=random.uniform(100.5, 999.5),
+               type=random.choice(CARS_TYPES),
+               producer=random.choice(CARS_PRODUCER),
+               mileage=random.uniform(0, 1000000),
+               )
 
     print ("CAR1: ", car1)
     print ("CAR2: ", car2)
 
-#CHECK UUID CHANGE
-    print ("------CHECK UUID CHANGE--------")
+    # CHECK UUID CHANGE
+    print ("\n------CHECK UUID CHANGE--------")
     old_number = car1.number
     car1.number = car1.change_UUID()
-    print("Change car number:",  car1.number)
     print("The number {} of car {} was changed on: {}".format(old_number, car1.producer, car1.number))
 
-#CHECK COMPARE
-    print ("-----CHECK COMPARE-------")
+    # CHECK COMPARE
+    print ("\n-----CHECK COMPARE-------")
     if car1.price <= car2.price:
-        print("price {} car {} biggest then price {} car {}".format(car2.price, car2.producer, car1.price, car1.producer))
+        print(
+            "price {} car {} biggest then price {} car {}".format(car2.price, car2.producer, car1.price, car1.producer))
     else:
-        print("price {} car {} biggest then price {} car {}".format(car1.price, car1.producer, car2.price, car2.producer))
+        print(
+            "price {} car {} biggest then price {} car {}".format(car1.price, car1.producer, car2.price, car2.producer))
 
     if car1.price < car2.price:
-        print("car {} price {} cheaper then car {} price {}".format(car1.producer, car1.price, car2.producer, car2.price))
+        print(
+            "car {} price {} cheaper then car {} price {}".format(car1.producer, car1.price, car2.producer, car2.price))
     else:
         print("car {} cheaper then car {}".format(car2.producer, car1.producer))
 
-
     if car1.price == car2.price:
-        print("price {} car {} is equal to price {} car {}".format(car1.price, car1.producer, car2.price, car2.producer))
-    else:
-        print("Bad compare")
+        print(
+            "price {} car {} is equal to price {} car {}".format(car1.price, car1.producer, car2.price, car2.producer))
 
     if car1.price != car2.price:
         print("price car {} isn't equal to price car {}".format(car1.producer, car2.producer))
-    else:
-       print("Bad compare")
 
-#CHECK GARAGE
-    print ("-----CHECK GARAGE------")
-    garages = []
 
-    for _ in range(random.randint(1, 3)):
-        count_cars = random.randint(1, 2)
-        random_place = random.randint(0,3)
+    print ("\n-----CHECK GARAGE------")
+    CESAR_NAME = ["Oleg", "Vitaliya", "Marina", "Grisha", "Petr"]
+    cesars = []
+    for cesar_counter in range(random.randint(2, 3)):
+        garages = []
+        for garage_counter in range(random.randint(1, 3)):
+            count_cars = random.randint(1, 4)
+            random_place = random.randint(0, 10)
 
-        get_car = []
+            get_car = []
+            for i in random.sample(cars, count_cars):
+                if i not in get_car:
+                    cars.remove(i)
+                    get_car.append(i)
 
-        for i in random.sample(cars,count_cars):
-            if i not in get_car:
-                cars.remove(i)
-                get_car.append(i)
+                if count_cars > random_place:
+                    raise ValueError("Count cars more then count place. RUN PGOGRAM AGAIN")
+                else:
+                    garage = Garage(town=random.choice(TOWNS),
+                                    places=random_place,
+                                    garage_cars=get_car)
+            garages.append(garage)
 
-            if count_cars > random_place:
-                raise ValueError("Count cars more then count place. RUN PGOGRAM AGAIN")
-            else:
-                garage = Garage(town = random.choice(TOWNS),
-                                places = random_place,
-                                garage_cars = get_car )
-                garages.append(garage)
-                print(garage)
-                print("COUNT CARS: ", len(get_car))
-                print("COUNT PLACE: ", random_place)
-                print("PRICE ALL CAR IN GARAGES: ", garage.hit_hat())
-        #print (garage)
+            #print("\nCOUNT CARS: ", len(get_car))
+            #print("COUNT PLACE: ", random_place)
+            #print("PRICE ALL CAR IN GARAGES: ", garage.hit_hat())
+            #print (garage)
 
+        print ("\n-----CHECK CESAR------")
+        cesar = Cesar(name=random.choice(CESAR_NAME), cesar_garages=garages)
+        cesars.append(cesar)
+
+        for item in cesars:
+            print(item, "\n")
+
+    print ("THE RICHEST CESAR IS: ",(Cesar.compare_cesar(cesars)).name)
+
+    print("\n----CHECK ADD CARS----------")
     print("BEFORE ADD NEW CAR: ", garages[0])
     print("PRICE ALL CARS IN GARAGE BEFORE ADD CAR: ", garages[0].hit_hat())
 
-    print("AFTER ADD NEW CAR: ", garages[0].add(car1))
+    print("\nAFTER ADD NEW CAR: ", garages[0].add(car1))
     print("PRICE ALL CARS IN GARAGE AFTER ADD CAR: ", garages[0].hit_hat())
 
-    print("BEFORE REMOVE NEW CAR: ", garages[0])
-    print("CAR FOR REMOVE: ", garages[0].cars[0])
-    print("AFTER REMOVE NEW CAR: ", garages[0].remove(garages[0].cars[0]))
+    print("\nBEFORE REMOVE NEW CAR: ", garages[0])
+    print("\nCAR FOR REMOVE: ", garages[0].cars[0])
+    print("\nAFTER REMOVE NEW CAR: ", garages[0].remove(garages[0].cars[0]))
 
-# CHECK CESAR
-    print ("-----CHECK CESAR------")
-
-    cesar = Cesar(name='Vasiliy', cesar_garages=garages)
-
-    print(cesar)
-    print("SUM PRICE ALL CAR: ", cesar.hit_hat())
-    print("COUNT CAR IN ALL GARAGES: ", cesar.сars_count())
-    print("ADD CAR TO FREE GARAGE: ", cesar.add_car(car1))
-    print("ADD CAR TO SELECTED GARAGE: ", cesar.add_car(car2, garages[0]))
-    print("COUNT OF GARAGE:", cesar.garages_count())
+    print("\nADD CAR TO FREE GARAGE: ", cesars[0].add_car(car1))
+    print("\nADD CAR TO SELECTED GARAGE: ", cesars[0].add_car(car2, garages[0]))
 
 
 print ("-----------------------------------")
